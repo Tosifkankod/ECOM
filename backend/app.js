b2b8393ec4ce7458b2186fffe1a1b6856e5c44bc
Tosif Kankod <tosifkankod146@gmail.com>
2023-09-16 22:45:32 +0530

    backend

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,11 @@ const errorMiddleware = require('./middleware/error');
 const bodyParser = require('body-parser');
 const fileUpload  = require('express-fileupload')
 const dotenv = require('dotenv')
-const path = require('path')
+const path = require('path');
+
 
 const app = express();
-
+ 
 // Config
 dotenv.config({path: "backend/config/config.env"})
 
@@ -30,14 +31,13 @@ app.use('/api/v1', user)
 app.use('/api/v1', order)
 app.use('/api/v1', payment)
 
-app.use(express.static(path.join(__dirname,"../frontend/build")))
+
+app.use(express.static(path.join(__dirname, "../backend/build")))
 
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"))
-})
-
-// Middleware for error
-app.use(errorMiddleware);
+    res.sendFile(path.resolve(__dirname, "../backend/build/index.html"))
+  
+})  
 
 
 module.exports = app; 
